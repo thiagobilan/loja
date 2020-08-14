dfcfb6caa5cf00bc923713e00bf395a411511634
Thiago Bilangieri <thiagobilan@gmail.com>
2020-08-14 00:37:39 -0300

    Adicionado o Provider

--- lib/common/custom_drawer/drawer_tile.dart
+++ lib/common/custom_drawer/drawer_tile.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:loja/models/page_manager.dart';
+import 'package:provider/provider.dart';
 
 class DrawerTile extends StatelessWidget {
   final IconData iconData;
@@ -9,9 +11,13 @@ class DrawerTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final int currentPage = context.watch<PageManager>().page;
+    final Color primaryColors = Theme.of(context).primaryColor;
     return InkWell(
       onTap: () {
-        debugPrint("toquei $page");
+        context.read<PageManager>().setPage(page);
+
+        debugPrint("toquei ${context.read<PageManager>().page}");
       },
       child: SizedBox(
         height: 60,
@@ -22,14 +28,14 @@ class DrawerTile extends StatelessWidget {
               child: Icon(
                 iconData,
                 size: 32,
-                color: Colors.grey[700],
+                color: currentPage == page ? primaryColors : Colors.grey[700],
               ),
             ),
             Text(
               title,
               style: TextStyle(
                 fontSize: 16,
-                color: Colors.grey[700],
+                color: currentPage == page ? primaryColors : Colors.grey[700],
               ),
             )
           ],
--- lib/screens/base/base_screen.dart
+++ lib/screens/base/base_screen.dart
@@ -1,35 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:loja/common/custom_drawer/custom_drawer.dart';
+import 'package:loja/models/page_manager.dart';
+import 'package:provider/provider.dart';
 
 class BaseScreen extends StatelessWidget {
   final PageController pageController = PageController();
   @override
   Widget build(BuildContext context) {
-    return PageView(
-      controller: pageController,
-      children: <Widget>[
-        Scaffold(
-          drawer: CustomDrawer(),
-          appBar: AppBar(
-            title: const Text('Home'),
+    return Provider(
+      create: (context) => PageManager(pageController),
+      child: PageView(
+        controller: pageController,
+        children: <Widget>[
+          Scaffold(
+            drawer: CustomDrawer(),
+            appBar: AppBar(
+              title: const Text('Home'),
+            ),
           ),
-        ),
-        Container(
-          color: Colors.red,
-          child: RaisedButton(
-            onPressed: () {
-              pageController.jumpToPage(1);
-            },
-            child: const Text('Proximo'),
+          Scaffold(
+            drawer: CustomDrawer(),
+            appBar: AppBar(
+              title: const Text('Home 2'),
+            ),
           ),
-        ),
-        Container(
-          color: Colors.yellow,
-        ),
-        Container(
-          color: Colors.green,
-        ),
-      ],
+          Scaffold(
+            drawer: CustomDrawer(),
+            appBar: AppBar(
+              title: const Text('Home 3'),
+            ),
+          ),
+          Scaffold(
+            drawer: CustomDrawer(),
+            appBar: AppBar(
+              title: const Text('Home 4'),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
